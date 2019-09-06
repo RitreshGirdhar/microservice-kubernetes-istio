06ae910d6cc876755caa173491220cfc97b5052d
RiteshGirdhar <ritresh.girdhar@gmail.com>
2019-09-06 16:06:12 +0530

    microservices

---
+++ ms2/src/main/java/com/microservices/ms2/Ms2Application.java
@@ -0,0 +1,13 @@
+package com.microservices.ms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms2Application.class, args);
+    }
+
+}
---
+++ ms2/src/test/java/com/microservices/ms2/Ms2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservices.ms2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ms2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
