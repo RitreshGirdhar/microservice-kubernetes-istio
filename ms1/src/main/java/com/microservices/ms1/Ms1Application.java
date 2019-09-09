0e3ba00aa095c7cdf8d1786834fc7de9710b2dad
RiteshGirdhar <ritresh.girdhar@gmail.com>
2019-09-09 18:20:29 +0530

    k8 example

--- ms1/src/main/java/com/microservices/ms1/Ms1Application.java
+++ ms1/src/main/java/com/microservices/ms1/Ms1Application.java
@@ -2,12 +2,21 @@ package com.microservices.ms1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Ms1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Ms1Application.class, args);
     }
+}
 
+@org.springframework.web.bind.annotation.RestController
+class RestController {
+
+    @GetMapping(path = "/api1")
+    public ResponseEntity<String> getInfo() {
+        return ResponseEntity.ok("hello");
+    }
 }
--- ms2/src/main/java/com/microservices/ms2/Ms2Application.java
+++ ms2/src/main/java/com/microservices/ms2/Ms2Application.java
@@ -2,6 +2,8 @@ package com.microservices.ms2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Ms2Application {
@@ -10,4 +12,13 @@ public class Ms2Application {
         SpringApplication.run(Ms2Application.class, args);
     }
 
+}
+
+@org.springframework.web.bind.annotation.RestController
+class RestController {
+
+    @GetMapping(path = "/api2")
+    public ResponseEntity<String> getInfo() {
+        return ResponseEntity.ok("hello");
+    }
 }
