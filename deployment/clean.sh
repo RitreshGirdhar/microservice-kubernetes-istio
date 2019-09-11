6ab5e69f06b310ce2e35a6b2301651826db80b92
RiteshGirdhar <ritresh.girdhar@gmail.com>
2019-09-11 10:00:30 +0530

    Final commit

--- deployment/clean.sh
+++ deployment/clean.sh
@@ -2,3 +2,5 @@ kubectl delete deployment k8ms1-v1
 kubectl delete deployment k8ms2-v1
 kubectl delete service k8ms1
 kubectl delete service k8ms2
+kubectl delete gateway k8-gateway
+kubectl delete virtualservice k8
